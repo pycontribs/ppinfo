d35031867b7c47fe49c047520ccc8ef331a2a7a7
Sorin Sbarnea <ssbarnea@redhat.com>
2022-08-06 13:43:50 +0100

    Add ability to identify min_python version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pinfo
+
+## Detect minimum version of python
+
+```python
+from ppinfo import Project
+p = Project(".")
+print(p.min_python)  # value from your setup.cfg
+```
--- test/test_ppinfo.py
+++ test/test_ppinfo.py
@@ -1,7 +1,14 @@
 """Tests for ppinfo package."""
+import pytest
+
+from ppinfo import Project
 
 
-def test_import() -> None:
-    """Test that we can import ppinfo."""
-    # pylint: disable=unused-import,import-outside-toplevel
-    import ppinfo
+@pytest.mark.parametrize(
+    ("path", "expected_python"),
+    [("test/fixtures/proj1", "3.7"), ("test/fixtures/py310", "3.10")],
+)
+def test_min_version(path: str, expected_python: str) -> None:
+    """Test ability to find minimal version of python required."""
+    project = Project(path)
+    assert project.min_python == expected_python
