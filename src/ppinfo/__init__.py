860e1d837ee853327b7704355ef1c23adf703d9d
Sorin Sbarnea <ssbarnea@redhat.com>
2022-08-06 12:42:39 +0100

    More bootstrap (#1)

---
+++ src/ppinfo/__init__.py
@@ -0,0 +1,1 @@
+"""ppinfo package."""
---
+++ test/test_ppinfo.py
@@ -0,0 +1,7 @@
+"""Tests for ppinfo package."""
+
+
+def test_import() -> None:
+    """Test that we can import ppinfo."""
+    # pylint: disable=unused-import,import-outside-toplevel
+    import ppinfo
